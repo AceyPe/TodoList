3f4ac2237fe9f15bdb83c4f6f5a374d71e7ef943
AceyPe <151679647+YahyaAdawy@users.noreply.github.com>
2024-01-07 17:59:44 +0200

    fixing the git

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const taskListItems = document.getElementsByClassName("taskList");
+
+  function addItem () {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `<p>${document.getElementById('todoInput').innerText}</p>
+    <button>delete</buttom>`
+  }
+
   return(
     <div className='all'>
-      <input type='text' placeholder='Add Todo...' className='todo'></input>
-      <button className='submitBtn'>submit</button>
+      <div className='todo'><input type='text' placeholder='Add Todo...' id='todoInput'></input>
+      <button className='submitBtn' onClick={addItem}>Submit</button></div>
       <div className='taskList'>
-        <li>hello</li>
+        
       </div>
     </div>
   );
